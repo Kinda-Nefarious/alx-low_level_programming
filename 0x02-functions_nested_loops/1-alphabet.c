3f73fdc1f327a206353c44edd35b58d73c05d69d
Kinda-Nefarious <rufaronyakudya@gmail.com>
2022-09-14 12:55:28 -0700

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints alphabet lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+
+	_putchar('\n');
+}
