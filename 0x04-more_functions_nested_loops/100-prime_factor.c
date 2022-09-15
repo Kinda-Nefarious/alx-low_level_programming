76959ac6a52a359e66c759b3f0e0a4ee524105ad
Kinda-Nefarious <rufaronyakudya@gmail.com>
2022-09-15 08:00:28 -0700

    Added corrections

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,22 +8,17 @@
 
 int main(void)
 {
+	long x, maxf;
 	long number = 612852475143;
-	int inc;
+	double square = sqrt(number);
 
-	while (inc++ < number / 2)
+	for (x = 1; x <= square; x++)
 	{
-		if (number % inc == 0)
+		if (number % x == 0)
 		{
-			number /= 2;
-			continue;
-		}
-
-		for (inc = 3; inc < number / 2; inc += 2)
-		{
-			if (number % inc == 0)
-				number /= inc;
-
+			maxf = number / x;
 		}
 	}
+	printf("%ld\n", maxf);
+	return (0);
 }
