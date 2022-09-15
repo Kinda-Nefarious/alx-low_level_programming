53825f8c687a4aa7e14e29807cb1a575d08844f7
Kinda-Nefarious <rufaronyakudya@gmail.com>
2022-09-15 07:00:55 -0700

    Added 7-print_diagonal.c correction

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,20 +9,22 @@
 
 void print_diagonal(int n)
 {
-	int len, space;
+	int x, y;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (len = 0; len < n; len++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
 		{
-			for (space = 0; space < len; space++)
-				_putchar(' ');
+			for (y = 0; y < x; y++)
+			{
+				_putchar(32);
+			}
 
-			_putchar('\\');
-
-			if (len == (n - 1))
-				continue;
-
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
