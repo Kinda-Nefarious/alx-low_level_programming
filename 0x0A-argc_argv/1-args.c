994ef7d97cb4ee62cead80e72e2c640fb8406dd1
Kinda-Nefarious <rufaronyakudya@gmail.com>
2022-09-30 19:23:45 -0700

    Added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;/*Ignore argv*/
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
