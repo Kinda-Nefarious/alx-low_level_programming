e223a360532506b53ad99413e189b0bceab1e921
Kinda-Nefarious <rufaronyakudya@gmail.com>
2022-10-12 10:07:13 -0700

    Added programs corrections

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int x;
 
 	if (array == NULL || action == NULL)
-		;
+	{
+		return;
+	}
 
 	for (x = 0; x < size; x++)
 	{
