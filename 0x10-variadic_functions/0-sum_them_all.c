7547baee188e1ec4d3e6d1bfc0993ad36d3e8294
Kinda-Nefarious <rufaronyakudya@gmail.com>
2022-10-13 06:30:53 -0700

    One for all, All for one

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all -a function that returns the sum of all its parameters
+ * @n: number of parameters
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	int sum = 0;
+	unsigned int x;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(valist, n);
+
+	for (x = 0; x < n; x++)
+	{
+		sum += va_arg(valist, int);
+	}
+
+	va_end(valist);
+	return (sum);
+}
